04049810453037e60229ea6ef6b1416928599301
optimus889 <132802487+optimus889@users.noreply.github.com>
2026-03-06 20:34:32 -0500

    Create database and fraud_transactions table

--- sql/athena_queries.sql
+++ sql/athena_queries.sql
@@ -1,3 +1,29 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS fraud_demo_db;
+
+--Create Table
+CREATE EXTERNAL TABLE IF NOT EXISTS fraud_demo_db.fraud_transactions (
+    transaction_id BIGINT,
+    timestamp STRING,
+    amount DOUBLE,
+    merchant STRING,
+    location STRING,
+    payment_method STRING,
+    fraud INT
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+    'separatorChar' = ',',
+    'quoteChar' = '"',
+    'escapeChar' = '\\'
+)
+STORED AS TEXTFILE
+LOCATION 's3://finalproject-fraud-detection/raw/'
+TBLPROPERTIES ('skip.header.line.count'='1');
+
+-- Test
+SELECT * FROM fraud_demo_db.fraud_transactions LIMIT 10;
+
 -- Fraud vs Normal transactions
 SELECT fraud, COUNT(*) AS transaction_count
 FROM fraud_demo_db.fraud_transactions
